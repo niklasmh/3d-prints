f268fb4696e0564c80567bbb0ae6bca9e0394045
Niklas Molnes Hole <nikasmh@hotmail.com>
2021-08-31 13:01:02 +0200

    Update kitchen-pincer hook

--- hook-for-kitchen-pincer/main.py
+++ hook-for-kitchen-pincer/main.py
@@ -6,9 +6,9 @@ cm = 10
 thickness = 0.2*cm
 z_fighting = 0.01
 width = 1*cm
-top_hook_height = 1.2*cm
+top_hook_height = 1.1*cm
 top_hook_depth = 0.6*cm
-bottom_hook_height = 0.7*cm
+bottom_hook_height = 0.8*cm
 bottom_hook_depth = top_hook_depth + thickness
 
 top_hook_hole = cube([top_hook_depth, thickness +
@@ -17,11 +17,18 @@ top_hook = translate([-thickness, z_fighting, -thickness])(
     cube([top_hook_depth + thickness*2, thickness, top_hook_height + thickness*2])
 ) - top_hook_hole
 
-bottom_hook_bottom = cube([bottom_hook_depth, thickness, thickness])
-bottom_hook = bottom_hook_bottom + \
-    translate([-thickness, 0, 0])(
+bottom_hook_bottom = cube(
+    [bottom_hook_depth+thickness/2**.5, thickness, thickness])
+bottom_hook = (
+    bottom_hook_bottom
+    + translate([-thickness, 0, 0])(
         cube([thickness, thickness, bottom_hook_height + thickness])
-    ) + translate([bottom_hook_depth - thickness, 0, 0.05*cm])(cube(thickness))
+    )
+    + translate([bottom_hook_depth, 0, 0.2*cm]
+                )(rotate([0, 45, 0])(cube(thickness)))
+    - translate([bottom_hook_depth+thickness/2**.5, 0, 0]
+                )(cube(thickness*2))
+)
 
 hook = (
     translate([0, 0, bottom_hook_height + thickness*2])(top_hook)
