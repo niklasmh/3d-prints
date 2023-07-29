e0ab7bedb879e4d2f1579d9e813d94146fc65245
Niklas Molnes Hole <nikasmh@hotmail.com>
2023-07-29 22:35:40 +0200

    Add magnetic night stand

---
+++ magnetic-night-stand/README.md
@@ -0,0 +1,15 @@
+# Attaching a magnet board to my nightstand
+
+## Problem
+
+Too much stuff on my nightstand.
+
+## Solution
+
+Add a magnet board to my headstand to offload some of the stuff.
+
+## Result
+
+Much more space on my nightstand! And I can easily customize my system.
+
+![Result](result.png)
---
+++ magnetic-night-stand/main.py
@@ -0,0 +1,32 @@
+from solid import *
+from solid.utils import *
+
+cm = 10
+
+width = 2*cm
+head_stand_width = 6.5*cm
+height = 3*cm
+gap = 0.1*cm
+m = 1*cm
+
+hook = linear_extrude(height=width, slices=1)(
+    polygon([
+        [0, 0],
+        [0, -height-m],
+        [m+gap+m, -height-m],
+        [m+gap+m, 0],
+        [m+gap+m+head_stand_width, 0],
+        [m+gap+m+head_stand_width, -height],
+        [m+gap+m+head_stand_width+m, -height],
+        [m+gap+m+head_stand_width+m, m],
+        [m+gap+m, m],
+        [m+gap, m],
+        [m+gap, -height],
+        [m, -height],
+        [m, 0],
+    ])
+)
+
+fn = 9
+scad_render_to_file(hook, "head-stand-magnet-board.scad",
+                    file_header='$fn = %d;' % fn)
